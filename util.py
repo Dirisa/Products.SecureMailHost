47f0c3979dbdd699ee7ce40521b0a11ab5294aa2
Andreas Jung <zopyxfilter@gmail.com>
2003-09-06 05:58:01 +0000

    - added check for email - added remove_dupes

--- util.py
+++ util.py
@@ -39,3 +39,43 @@ def users_for_local_role(obj, userids, role):
             changed = 1
             add_local_role(obj, u, role)
     return changed
+
+
+def isValidEmailAddress(email):
+    """ validate an email address """
+
+    at = email.find('@')
+    pt = email.find('.')
+    # Minimal lenght must be 8
+    if len(email) <= 7 :
+        return 0
+    # We must have an @ but not first and not last
+    if at == -1 or at == 0 or email[-1] == '@':
+        return 0
+    # Same applies for the point
+    if pt == -1 or pt == 0 or email[-1] == '.':
+        return 0
+    # Only one @ is valid
+    if email.find('@',at+1) > -1:
+        return 0
+    # @ and . cannot be together
+    if email[at-1] == '.' or email[at+1] == '.':
+        return 0
+    # subdomain must have at least 2 letters
+    if email[at+2] == '.':
+        return 0
+    # no spaces allowed
+    if email.find(' ') > -1:
+        return 0
+    return 1
+
+
+def remove_dupes(lst):
+    """ remove dupes from a list """
+
+    ret = []
+    for l in lst:
+        if not l in ret:
+            ret.append(l)
+    return ret 
+
