d58fc3a4670f71714fe931e3b31857fc1a28ed3a
Andreas Jung <zopyxfilter@gmail.com>
2003-09-07 06:45:02 +0000

    updated

--- issue_schema.py
+++ issue_schema.py
@@ -1,15 +1,41 @@
 from Products.Archetypes.public import BaseSchema, Schema, DisplayList
 from Products.Archetypes.public import StringField, TextField, IntegerField
 from Products.Archetypes.public import SelectionWidget, TextAreaWidget
-from Products.Archetypes.public import RichWidget
-
-VOC_LIMIT_FOLLOWUPS = DisplayList((
-  (1, 'Yes'),
-  (0, 'No'),                                       
-))
 
 schema = BaseSchema +  Schema((
     StringField('description',
                 searchable=1,
+                ),
+    StringField('progress_hours_estimated',
+                searchable=1,
+                schemata='progress'
+                ),
+    StringField('progress_hours_needed',
+                searchable=1,
+                schemata='progress'
+                ),
+    StringField('progress_percent_done',
+                searchable=1,
+                schemata='progress'
+                ),
+    StringField('contact_name',
+                searchable=1,
+                schemata='contact'
+                ),
+    StringField('contact_address',
+                searchable=1,
+                schemata='contact'
+                ),
+    StringField('contact_email',
+                searchable=1,
+                schemata='contact'
+                ),
+    StringField('contact_phone',
+                searchable=1,
+                schemata='contact'
+                ),
+    StringField('contact_fax',
+                searchable=1,
+                schemata='contact'
                 ),
     ))
