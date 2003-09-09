fb31f7393b190f8f93097f38e4b05635793a5ee7
Andreas Jung <zopyxfilter@gmail.com>
2003-09-09 19:07:51 +0000

    added security assertions

--- SchemaEditor.py
+++ SchemaEditor.py
@@ -5,11 +5,14 @@ PloneCollectorNG - A Plone-based bugtracking system
 
 Published under the Zope Public License
 
-$Id: SchemaEditor.py,v 1.10 2003/09/09 16:16:25 ajung Exp $
+$Id: SchemaEditor.py,v 1.11 2003/09/09 19:07:51 ajung Exp $
 """
 
 import operator
 
+from Globals import InitializeClass
+from AccessControl import ClassSecurityInfo
+from Products.CMFCore import CMFCorePermissions
 from BTrees.OOBTree import OOBTree
 from Products.Archetypes.Schema import Schema
 from Products.Archetypes.public import DisplayList
@@ -17,10 +20,14 @@ from Products.Archetypes.Field import *
 from Products.Archetypes.Widget import *
 
 import util
+from config import ManageCollector
 
 class SchemaEditor:
     """ a simple TTW editor for Archetypes schemas """
 
+    security = ClassSecurityInfo()
+
+    security.declareProtected(ManageCollector, 'schema_init')
     def schema_init(self, schema):
         self._schema_names = []    # list of schemata names
         self._schemas = OOBTree()  # map schemata name to schemata
@@ -31,6 +38,7 @@ class SchemaEditor:
                 self._schemas[field.schemata] = Schema(field.schemata)
             self._schemas[field.schemata].addField(field)
 
+    security.declareProtected(ManageCollector, 'getWholeSchema')
     def getWholeSchema(self):
         """ return the concatenation of all schemas """       
         l = [self._schemas[name] for name in self._schema_names]
@@ -44,14 +52,17 @@ class SchemaEditor:
                 field.accessor = 'archetypes_accessor'
         return s
 
+    security.declareProtected(ManageCollector, 'getSchemaNames')
     def getSchemaNames(self):
         """ return names of all schematas """
         return self._schema_names
 
+    security.declareProtected(ManageCollector, 'getSchema')
     def getSchema(self, name):
         """ return a schema given by its name """
         return self._schemas[name]
 
+    security.declareProtected(ManageCollector, 'newSchema')
     def newSchema(self, fieldset, RESPONSE=None):
         """ add a new schema """
         if fieldset in self._schema_names:
@@ -62,17 +73,20 @@ class SchemaEditor:
 
         util.redirect(RESPONSE, 'pcng_schema_editor', 'Schema added', fieldset=fieldset)
 
+    security.declareProtected(ManageCollector, 'delSchema')
     def delSchema(self, fieldset, RESPONSE=None):
         """ delete a schema """
         self._schema_names.remove(fieldset)
         del self._schemas[fieldset]
         util.redirect(RESPONSE, 'pcng_schema_editor', 'Schema deleted', fieldset=fieldset)
 
+    security.declareProtected(ManageCollector, 'schema_del_field')
     def schema_del_field(self, fieldset, name, RESPONSE=None):
         """ remove a field from a fieldset """
         del self._schemas[fieldset][name]
         util.redirect(RESPONSE, 'pcng_schema_editor', 'Field deleted', fieldset=fieldset)
 
+    security.declareProtected(ManageCollector, 'schema_update')
     def schema_update(self, REQUEST, RESPONSE=None):
         """ update a schema schema """
 
@@ -80,9 +94,12 @@ class SchemaEditor:
         fieldset = R['fieldset'] 
 
         if R.has_key('schema_add_field'):
-            schema = self._schemas[fieldset]
-            field = StringField(R['name'], schemata=fieldset, widget=StringWidget)
-            schema.addField(field)
+            fields = self._schemas[fieldset].fields()
+            fields.append(StringField(R['name'], schemata=fieldset, widget=StringWidget))
+            print fields
+            schema = Schema(fieldset)
+            for f in fields:
+                schema.addField(f)
             self._schemas[fieldset] = schema
 
             util.redirect(RESPONSE, 'pcng_schema_editor', 'Field added', fieldset=fieldset)
@@ -92,8 +109,10 @@ class SchemaEditor:
 
         fieldnames = [name for name in R.keys()  if not isinstance(R[name], str) ]
         fieldnames.sort(lambda a,b,R=R: cmp(R[a].order, R[b].order))
+        print fieldnames
 
         for name in fieldnames:
+            print name
             d =  R[name]
 
             if d.field == 'StringField': field = StringField
@@ -110,16 +129,16 @@ class SchemaEditor:
             D['schemata'] = fieldset
          
             if d.widget == 'String': pass
-            elif d.widget == 'Select': D['widget'] = SelectionWidget(format='select')
-            elif d.widget == 'Radio': D['widget'] = SelectionWidget
-            elif d.widget == 'Textarea': D['widget'] = TextAreaWidget
-            elif d.widget == 'Calendar': D['widget'] = CalendarWidget
-            elif d.widget == 'Boolean': D['widget'] = BooleanWidget
-            elif d.widget == 'MultiSelect': D['widget'] = MultiSelectionWidget
-            elif d.widget == 'Keywords': D['widget'] = KeywordWidget
-            elif d.widget == 'Richtext': D['widget'] = RichWidget
-            elif d.widget == 'Password': D['widget'] = PasswordWidget
-            elif d.widget == 'Visual': D['widget'] = VisualWidget
+            elif d.widget == 'Select':      widget = SelectionWidget(format='select', visible=d.visible)
+            elif d.widget == 'Radio':       widget = SelectionWidget(visible=d.visible)
+            elif d.widget == 'Textarea':    widget = TextAreaWidget(visible=d.visible)
+            elif d.widget == 'Calendar':    widget = CalendarWidget(visible=d.visible)
+            elif d.widget == 'Boolean':     widget = BooleanWidget(visible=d.visible)
+            elif d.widget == 'MultiSelect': widget = MultiSelectionWidget(visible=d.visible)
+            elif d.widget == 'Keywords':    widget = KeywordWidget(visible=d.visible)
+            elif d.widget == 'Richtext':    widget = RichWidget(visible=d.visible)
+            elif d.widget == 'Password':    widget = PasswordWidget(visible=d.visible)
+            elif d.widget == 'Visual':      widget = VisualWidget(visible=d.visible)
             else:
                 raise ValueError('unknown widget type: %s' % d.widget)
 
@@ -147,10 +166,12 @@ class SchemaEditor:
 
         util.redirect(RESPONSE, 'pcng_schema_editor', 'Schema changed', fieldset=fieldset)
 
+    security.declareProtected(ManageCollector, 'schema_get_fieldtype')
     def schema_get_fieldtype(self, field):
         """ return the type of a field """
         return field.__class__.__name__
     
+    security.declareProtected(ManageCollector, 'schema_format_vocabulary')
     def schema_format_vocabulary(self, field):
         """ format the DisplayList of a field to be display
             within a textarea.
