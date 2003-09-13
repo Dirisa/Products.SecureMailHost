5345cb729e3c43d5ead49e494bbba018910589b6
Kapil Thangavelu <k_vertigo@users.sourceforge.net>
2003-09-13 15:37:49 +0000

    New repository initialized by cvs2svn.

--- skins/plone_help_center/getSyndicationURL.py
+++
@@ -1,2 +0,0 @@
-##parameters=portal_url, my_path, subtypes
-return portal_url + '/search_rss?sort_on=modified&sort_order=descending&path=' + my_path + '&' + ('&'.join(['portal_type=%s' % s for s in subtypes ]))
