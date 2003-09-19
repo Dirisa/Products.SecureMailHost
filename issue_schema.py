f836f825171d05a4a1a887e1f04a80076dc5daf5
Andreas Jung <zopyxfilter@gmail.com>
2003-09-19 14:51:54 +0000

    added label, label_msgid and i18n_domain

--- issue_schema.py
+++ issue_schema.py
@@ -5,13 +5,26 @@ PloneCollectorNG - A Plone-based bugtracking system
 
 Published under the Zope Public License
 
-$Id: issue_schema.py,v 1.8 2003/09/14 11:55:47 ajung Exp $
+$Id: issue_schema.py,v 1.9 2003/09/19 14:51:54 ajung Exp $
 """
 
 from OrderedSchema import OrderedSchema 
 from Products.Archetypes.public import DisplayList
 from Products.Archetypes.public import StringField, TextField, IntegerField
 from Products.Archetypes.public import SelectionWidget, TextAreaWidget, IdWidget, StringWidget
+
+VOCAB_TOPIC = DisplayList((
+  ('UI', 'UI'),
+  ('Backend', 'Backend'),
+  ('Others', 'Others')
+))
+
+VOCAB_IMPORTANCE = DisplayList((
+  ('low', 'Low'),
+  ('medium', 'Medium'),
+  ('high', 'High'),
+  ('critical', 'Critical'),
+))
 
 schema = OrderedSchema((
     StringField('id',
@@ -25,6 +38,7 @@ schema = OrderedSchema((
                                 description_msgid="help_name",
                                 i18n_domain="plone"),
                 ),
+
     StringField('title',
                 required=1,
                 searchable=1,
@@ -36,6 +50,7 @@ schema = OrderedSchema((
                                     i18n_domain="plone"),
                 ),
     StringField('description',
+                required=1,
                 searchable=1,
                 schemata='issuedata',
                 widget=TextAreaWidget,
@@ -43,40 +58,87 @@ schema = OrderedSchema((
     StringField('solution',
                 searchable=1,
                 schemata='issuedata',
-                widget=TextAreaWidget,
+                widget=TextAreaWidget(label='Solution',
+                                      label_msgid='label_solution',
+                                      i18n_domain='plonecollectorng'),
                 ),
+    StringField('topic',
+                required=1,
+                searchable=1,
+                schemata='issuedata',
+                vocabulary=VOCAB_TOPIC,
+                widget=SelectionWidget(format='select',
+                                       label='Topic',
+                                       label_msgid='label_topic',
+                                       i18n_domain='plonecollectorng'),
+                ),
+    StringField('importance',
+                required=1,
+                searchable=1,
+                schemata='issuedata',
+                vocabulary=VOCAB_IMPORTANCE,
+                widget=SelectionWidget(format='select',
+                                       label='Importance',
+                                       label_msgid='label_importance',
+                                       i18n_domain='plonecollectorng'),
+                ),
+
     StringField('progress_hours_estimated',
-                schemata='progress'
+                schemata='progress',
+                widget=StringWidget(label='Hours estimated',
+                                    label_msgid='label_hours_estimated',
+                                    i18n_domain='plonecollectorng'),
                 ),
     StringField('progress_hours_needed',
-                schemata='progress'
+                schemata='progress',
+                widget=StringWidget(label='Hours needed',
+                                    label_msgid='label_hours_needed',
+                                    i18n_domain='plonecollectorng'),
                 ),
     StringField('progress_percent_done',
-                schemata='progress'
+                schemata='progress',
+                widget=StringWidget(label='Percent done',
+                                    label_msgid='label_percent_done',
+                                    i18n_domain='plonecollectorng'),
                 ),
     # do not remove 'contact_name'
     StringField('contact_name',
                 searchable=1,
                 required=1,
-                schemata='contact'
+                schemata='contact',
+                widget=StringWidget(label='Name',
+                                    label_msgid='label_contact_name',
+                                    i18n_domain='plonecollectorng'),
                 ),
     # do not remove 'contact_email'
     StringField('contact_email',
                 searchable=1,
                 required=1,
                 schemata='contact',
-                validators=('isEmail',)
+                validators=('isEmail',),
+                widget=StringWidget(label='E-Mail',
+                                    label_msgid='label_contact_email',
+                                    i18n_domain='plonecollectorng'),
                 ),
     StringField('contact_address',
                 searchable=1,
-                schemata='contact'
+                schemata='contact',
+                widget=StringWidget(label='Address',
+                                    label_msgid='label_contact_address',
+                                    i18n_domain='plonecollectorng'),
                 ),
     StringField('contact_phone',
                 searchable=1,
-                schemata='contact'
+                schemata='contact',
+                widget=StringWidget(label='Phone',
+                                    label_msgid='label_contact_phone',
+                                    i18n_domain='plonecollectorng'),
                 ),
     StringField('contact_fax',
                 searchable=1,
-                schemata='contact'
+                schemata='contact',
+                widget=StringWidget(label='Fax',
+                                    label_msgid='label_contact_fax',
+                                    i18n_domain='plonecollectorng'),
                 ),
     ))
