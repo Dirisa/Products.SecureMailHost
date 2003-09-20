52809d318722bdc697771870aa7d05238ff76820
Andreas Jung <zopyxfilter@gmail.com>
2003-09-20 19:54:46 +0000

    removed

--- skins/plonecollectorng/pcng_format_action.py
+++
@@ -1,9 +0,0 @@
-##parameters=action
-
-# Reformat a workflow action to be displayed
-
-action = action.capitalize()
-action = action.replace('_', ' ')
-
-if action == 'Restrict accepted': return 'Restrict'
-return action 
