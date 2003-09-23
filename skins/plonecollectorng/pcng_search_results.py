e198dfe579b4be658af993ee0a4635310629fa9e
Andreas Jung <zopyxfilter@gmail.com>
2003-09-23 15:13:44 +0000

    savepoint

--- skins/plonecollectorng/pcng_search_results.py
+++ skins/plonecollectorng/pcng_search_results.py
@@ -1,7 +1,8 @@
-
 # perform a search
 
 R = context.REQUEST
+sort_on = R.get('sort_on', 'getId')
+sort_direction = R.get('sort_direction', 'desc')
 query = {}
 
 def toQuery(key):
@@ -9,16 +10,32 @@ def toQuery(key):
         query[key]= R[key]
 
 
+def query_sort(a, b):
+    """ helper method to sort the result list """
+
+    va = getattr(a, sort_on); vb = getattr(b, sort_on)
+
+    # try to convert the values to ints
+    try:
+        va = int(va); vb = int(vb)
+    except: pass
+    if sort_direction == 'desc': return -1 * cmp(va, vb)
+    else: return cmp(va, vb)  
+
+# here starts the show
+
 toQuery('SearchableText')
 toQuery('status')
 toQuery('Creator')
 toQuery('assigned_to')
 
-
+# Default query
 if len(query) == 0:
     query['status'] = ('Accepted', 'Pending')
 
-print query
-results = context.pcng_catalog.searchResults(query)
-print results
+results = list(context.pcng_catalog.searchResults(query))
+
+if sort_on:
+    results.sort(query_sort)
+
 return results
--- skins/plonecollectorng/pcng_uniquevalsFor.py
+++ skins/plonecollectorng/pcng_uniquevalsFor.py
@@ -1,5 +1,7 @@
 ##parameters=key
 
+# retrieve the unique values from the catalog for the given key
+
 values = context.pcng_catalog.uniqueValuesFor(key)
 values = [v for v in values if v]
 values.sort()
