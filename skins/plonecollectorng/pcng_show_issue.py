96b794a95822e40c2b78cc0a6823d055000d7c70
Andreas Jung <zopyxfilter@gmail.com>
2003-09-23 13:32:03 +0000

    added

---
+++ skins/plonecollectorng/pcng_show_issue.py
@@ -0,0 +1,10 @@
+##parameters=issuenumber
+
+# Jump to the view method of an issue given by its number
+
+if hasattr(context, issuenumber):
+    url = context[issuenumber].absolute_url()
+    context.REQUEST.RESPONSE.redirect(url)
+else:
+    raise ValueError('There is no issue #%s' % issuenumber)
+
