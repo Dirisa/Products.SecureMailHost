35bf3ba3d891ab40124544befa11e66850a1cd32
Andreas Jung <zopyxfilter@gmail.com>
2003-10-03 09:00:18 +0000

    fixed setDefaultRoles()

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ PloneCollectorNG - A Plone-based bugtracking system
 
 Published under the Zope Public License
 
-$Id: config.py,v 1.6 2003/09/20 12:42:10 ajung Exp $
+$Id: config.py,v 1.7 2003/10/03 09:00:18 ajung Exp $
 """
 
-from Products.CMFCore.CMFCorePermissions import AddPortalContent
+from Products.CMFCore.CMFCorePermissions import AddPortalContent, setDefaultRoles
 
 ADD_CONTENT_PERMISSION = AddPortalContent
 PROJECTNAME = "PloneCollectorNG"
@@ -25,3 +25,8 @@ EditCollectorIssue = 'Edit PloneCollectorNG issue'
 AddCollectorIssue = 'Add PloneCollectorNG issue'
 AddCollectorIssueFollowup = 'Add PloneCollectorNG issue followup'
 
+# create new permissions and pre-assign roles 
+setDefaultRoles(EditCollectorIssue, ('Manager', 'TrackerAdmin'))
+setDefaultRoles(AddCollectorIssue, ('Manager', 'TrackerAdmin'))
+setDefaultRoles(AddCollectorIssueFollowup, ('Manager', 'TrackerAdmin'))
+setDefaultRoles(ManageCollector, ('Manager', 'TrackerAdmin'))
