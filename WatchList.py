b70c72862763b2b6cda15c0938e69174ed78de3e
Andreas Jung <zopyxfilter@gmail.com>
2003-10-11 14:56:13 +0000

    using PersistentList

--- WatchList.py
+++ WatchList.py
@@ -5,16 +5,17 @@ PloneCollectorNG - A Plone-based bugtracking system
 
 Published under the Zope Public License
 
-$Id: WatchList.py,v 1.2 2003/09/08 04:38:42 ajung Exp $
+$Id: WatchList.py,v 1.3 2003/10/11 14:56:13 ajung Exp $
 """
 
 from Globals import InitializeClass
+from ZODB.PersistentList import PersistentList
 
 class WatchList:
     """ mix-in class for watchlist handling """
 
     def wl_init(self):
-        self._watchers = []
+        self._watchers = PersistentList()
 
     def wl_getWatchers(self):
         return self._watchers
