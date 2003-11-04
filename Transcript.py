e2c98f9de8fc5d36bcc90e5d315f6c2476132afb
Andreas Jung <zopyxfilter@gmail.com>
2003-11-04 13:44:55 +0000

    misc fixes and changes for migration script

--- Transcript.py
+++ Transcript.py
@@ -5,7 +5,7 @@ PloneCollectorNG - A Plone-based bugtracking system
 
 License: see LICENSE.txt
 
-$Id: Transcript.py,v 1.15 2003/11/01 17:03:26 ajung Exp $
+$Id: Transcript.py,v 1.16 2003/11/04 13:44:55 ajung Exp $
 """
 
 import time 
@@ -25,10 +25,17 @@ class TranscriptEvent(Persistent, Implicit):
 
     def __init__(self, type, **kw):
         self.type = type
-        self.created = time.time()
-        self.user = util.getUserName()
+
         for k,v in kw.items():
             setattr(self, k, v)
+        
+        created = kw.get('created', None)
+        if created is not None: self.created = created
+        else: self.created = time.time()
+
+        user = kw.get('user', None)
+        if user: self.user = user
+        else: self.user = util.getUserName()
 
     def __str__(self):
         return 'TranscriptEvent(%s)' % ','.join(['%s=%s' % (k,v) for k,v in self.__dict__.items()])
@@ -66,45 +73,49 @@ class Transcript(Persistent, Implicit):
 
     security.declareProtected(CMFCorePermissions.View, 'add')
     def add(self, event):
-        self._items[event.created] = event
+        created = event.created
+        while self._items.has_key(created):
+            created += 0.01
+
+        self._items[created] = event
 
     replace = add  # make an alias to provide support to replace an event
 
     security.declareProtected(CMFCorePermissions.View, 'addComment')
-    def addComment(self, comment):
-        event = TranscriptEvent('comment', comment=comment)
+    def addComment(self, comment, user=None, created=None):
+        event = TranscriptEvent('comment', comment=comment, user=user, created=created)
         self.add(event)
 
     security.declareProtected(CMFCorePermissions.View, 'addChange')
-    def addChange(self, field, old, new):
+    def addChange(self, field, old, new, user=None, created=None):
         if str(old) != str(new):
-            event = TranscriptEvent('change', field=field, old=old, new=new)
+            event = TranscriptEvent('change', field=field, old=old, new=new, user=user, created=created)
             self.add(event)
 
     security.declareProtected(CMFCorePermissions.View, 'addIncrementalChange')
-    def addIncrementalChange(self, field, old, new):
+    def addIncrementalChange(self, field, old, new, user=None, created=None):
 
 #        assert isinstance(old, (list, tuple)) and isinstance(new, (list, tuple))
         added = list(difference(OOSet(new), OOSet(old)))
         removed = list(difference(OOSet(old), OOSet(new)))
 
         if removed or added:
-            event = TranscriptEvent('incrementalchange', field=field, added=added, removed=removed)
+            event = TranscriptEvent('incrementalchange', field=field, added=added, removed=removed, user=user, created=created)
             self.add(event)
 
     security.declareProtected(CMFCorePermissions.View, 'addReference')
-    def addReference(self, tracker, ticketnum, comment):
-        event = TranscriptEvent('reference', tracker=tracker, ticketnum=ticketnum, comment=comment)
+    def addReference(self, tracker, ticketnum, comment, user=None, created=None):
+        event = TranscriptEvent('reference', tracker=tracker, ticketnum=ticketnum, comment=comment, user=user, created=created)
         self.add(event)
 
     security.declareProtected(CMFCorePermissions.View, 'addUpload')
-    def addUpload(self, fileid, comment):
-        event = TranscriptEvent('upload', fileid=fileid, comment=comment)
+    def addUpload(self, fileid, comment, user=None, created=None):
+        event = TranscriptEvent('upload', fileid=fileid, comment=comment, user=user, created=created)
         self.add(event)
 
     security.declareProtected(CMFCorePermissions.View, 'addAction')
-    def addAction(self, action):
-        event = TranscriptEvent('action', action=action)
+    def addAction(self, action, user=None):
+        event = TranscriptEvent('action', action=action, user=user)
         self.add(event)
 
     security.declareProtected(CMFCorePermissions.View, 'getEvents')
