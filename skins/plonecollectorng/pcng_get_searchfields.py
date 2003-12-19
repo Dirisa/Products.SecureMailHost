84cde96a900119a14fff996c0843fe98f067af74
Andreas Jung <zopyxfilter@gmail.com>
2003-12-19 16:07:21 +0000

    misc searchform ed updates

--- skins/plonecollectorng/pcng_get_searchfields.py
+++ skins/plonecollectorng/pcng_get_searchfields.py
@@ -1,18 +1,6 @@
 
 # return a sequence of searchable fields
 
-names = ['status', 'creator', 'assigned_to', 'importance', 'topic']
-
-schema = context.atse_getSchema()
-fields = []
-
-for n in names:
-
-    try: 
-        field = schema[n]
-        fields.append( (n, field) )
-    except:
-        fields.append( (n, None) )
-        pass
-
-return fields
+names = ['status', 'Creator', 'assigned_to', 'importance', 'topic']
+names.extend([f.getName() for f in context.atse_getSchema().fields() if getattr(f, 'createindex', 0) == 1])                                
+return names
