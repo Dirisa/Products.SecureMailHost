b2d11ef1757326c3af3a391559e753ecb31b3582
Andreas Jung <zopyxfilter@gmail.com>
2004-01-15 17:30:45 +0000

    added translation support

--- skins/plonecollectorng/format_transcript.py
+++ skins/plonecollectorng/format_transcript.py
@@ -1,20 +1,36 @@
+##parameters=language=None
 
-# Format the issue for email notification
-
-def nl(text=''):
-    l.append(text)
-
-def indent_block(text):
-    return '\n'.join(['    %s' % line    for line in text.split('\n')])
+# Format the issue for email notification.
+# The return format is UNICODE !
 
 import DateTime
 
+# Wrapper for translate()
+def TR(id, default):
+    return context.translate(id, default, language)
+
+# convert string to unicode and append to list
+def nl(text=''):
+    if same_type(text, u''):
+        l.append(text)
+    else:
+        l.append(unicode(text, site_encoding))
+
+# a simple indenter for text blocks
+def indent_block(text):
+    return '\n'.join(['    %s' % line    for line in text.split('\n')])
+
 l = []
 
-nl('Issue: #%s: %s' % (context.getId(), context.Title()))
-nl('Topic: %s' % (context.topic))
-nl('Status: %s, Importance: %s, Classification: %s' % (context.status(), context.importance, context.classification))
-nl('Ticket URL: http://%s/%s' % (context.aq_parent.canonical_hostname, context.absolute_url(1)))
+# Determine site encoding
+site_encoding = context.getSiteEncoding()
+
+nl('%s: #%s: %s' % (TR('issue', 'Issue'), context.getId(), context.Title()))
+nl('%s: %s' % (TR('topic', 'Topic'),context.topic))
+nl('%s: %s, %s: %s, %s: %s' % (TR('status', 'Status'), context.status(), 
+                               TR('importance','Importance'), context.importance, 
+                               TR('classification', 'Classification'), context.classification))
+nl('%s URL: http://%s/%s' % (TR('issue', 'Issue'), context.aq_parent.canonical_hostname, context.absolute_url(1)))
 nl('-'*75 + '\n') 
 
 n = 0
@@ -22,25 +38,37 @@ groups = context.getTranscript().getEventsGrouped()
 for group in groups:
     datestr = context.toLocalizedTime(DateTime.DateTime(group[0].created), long_format=1)
     uid = group[0].user
-    nl('#%d %s %s (%s) ----------------------------' % (len(groups)-n, context.lastAction().capitalize(), datestr, uid)) 
+
+    # Find action in current group
+    action = ''
     for ev in group:
+        if ev.type == 'action': action = ev.action
+        
+    nl('#%d %s %s (%s) ----------------------------' % (len(groups)-n, action, datestr, uid)) 
+
+    for ev in group:
+
         if ev.type == 'comment':
-            nl('Comment:')
+            nl('%s:' % TR('comment', 'Comment'))
             nl(indent_block(ev.comment))
+
         elif ev.type == 'change':
-            nl('Changed: %s: "%s" -> "%s"' % (ev.field, ev.old, ev.new))
+            nl('%s: %s: "%s" -> "%s"' % (TR('change', 'Change'), ev.field, ev.old, ev.new))
+
         elif ev.type == 'incrementalchange':
-            nl('Changed: %s: added: %s , removed: %s' % (ev.field, ev.added, ev.removed))
+            nl('%s: %s: %s: %s , %s: %s' % (TR('changed', 'Changed'), ev.field, 
+                                            TR('added', 'Added'), ev.added, 
+                                            TR('removed', 'Removed'), ev.removed))
         elif ev.type == 'reference':
-            nl('Reference: %s: %s/%s (%s)' % (ev.tracker, ev.ticketnum, ev.comment))
+            nl('%s: %s: %s/%s (%s)' % (TR('reference', 'Reference'), ev.tracker, ev.ticketnum, ev.comment))
+
         elif ev.type == 'upload':
-            s = 'Upload: %s/%s ' % (context.absolute_url(), ev.fileid)
+            s = '%s: %s/%s ' % (TR('upload', 'Upload'), context.absolute_url(), ev.fileid)
             if ev.comment:
                 s+= ' (%s)' % ev.comment
             nl(s)
 
     n+=1; nl()
 
-return '\n'.join(l), 'iso-8859-15'
+return '\n'.join(l)
                                      
-
