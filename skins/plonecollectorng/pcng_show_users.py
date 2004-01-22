2b34f6c644bd8d8e8f114891abb50e37df4079df
Andreas Jung <zopyxfilter@gmail.com>
2004-01-22 19:55:41 +0000

    showing memberdata properties

--- skins/plonecollectorng/pcng_show_users.py
+++ skins/plonecollectorng/pcng_show_users.py
@@ -5,6 +5,8 @@ mbrtool = context.portal_membership
 users = context.acl_users.getUserNames()
 users.sort()
 
+context.REQUEST.RESPONSE.setHeader('content-type', 'text/plain; charset=utf-8')
+
 collectors = [ c.getObject() for c in context.portal_catalog(meta_type="PloneCollectorNG") ]
 
 for user in users:
@@ -20,6 +22,13 @@ for user in users:
         roles = [r for r in roles  if r not in ('Authenticated', 'Member') ]
         if roles:
             print "\t", c.getId(), roles
+
+        print
+
+        ids = context.portal_memberdata.propertyIds(); ids.sort()
+        for id in ids:
+            v = member.getProperty(id)
+            print"\t%30s = %s (unicode=%s)" % (id, v, same_type(v, u''))
 
     print 
 
