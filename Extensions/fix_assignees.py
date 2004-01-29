d4b42c09a7c09d6338cf61939b8f6c45a006996b
Andreas Jung <zopyxfilter@gmail.com>
2004-01-29 09:24:59 +0000

    added

---
+++ Extensions/fix_assignees.py
@@ -0,0 +1,11 @@
+def fix(self):
+    wftool = self.portal_workflow  # evil
+    for issue in self.objectValues('PloneIssueNG'):
+        print issue.getId(), issue.assigned_to(), issue.status(), issue.validActions()
+        events = issue.getTranscript().getEvents()
+        for ev in events:
+            if ev.getType()=='incrementalchange' and ev.getValue('field')=='Assignees':
+                people = ev.getValue('added')
+                print people
+                issue.issue_followup('assign', 'created by migraiton tool', assignees=people)
+    return 'done'
