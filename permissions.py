dd5a28d88d971510e0b84027c44c4a9102892248
Kapil Thangavelu <k_vertigo@users.sourceforge.net>
2004-02-10 19:37:19 +0000

    New repository initialized by cvs2svn.

--- permissions.py
+++
@@ -1,9 +0,0 @@
-"""
-$Id: permissions.py,v 1.2 2005/03/11 11:59:14 optilude Exp $
-"""
-from Products.CMFCore.CMFCorePermissions import setDefaultRoles
-
-ADD_CONTENT_PERMISSION = 'Add PloneSoftwareCenter Content'
-
-# Let members add new packages for review
-setDefaultRoles(ADD_CONTENT_PERMISSION, ('Member', 'Manager','Owner',))
