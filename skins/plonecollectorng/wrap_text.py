e0f4f1819a3700cbb335707e5a8283a731f144fd
Andreas Jung <zopyxfilter@gmail.com>
2004-02-11 17:14:02 +0000

    added

---
+++ skins/plonecollectorng/wrap_text.py
@@ -0,0 +1,17 @@
+##parameters=text,cols=120,indent=0
+
+# Wrap and indent text
+
+from textwrap import wrap
+
+lst = []
+
+for line in text.split('\n'):
+    for l in wrap(line, int(cols)):
+        lst.append(l)
+
+if indent:
+    for i in range(len(lst)):
+        lst[i] = (' '*int(indent)) + lst[i]
+
+return '\n'.join(lst)
