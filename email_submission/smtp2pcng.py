994e82612514cf7949cd643ed1f2b5fad551d9df
Andreas Jung <zopyxfilter@gmail.com>
2004-02-28 10:22:05 +0000

    *** empty log message ***

--- email_submission/smtp2pcng.py
+++ email_submission/smtp2pcng.py
@@ -7,12 +7,14 @@ PloneCollectorNG - A Plone-based bugtracking system
 
 License: see LICENSE.txt
 
-$Id: smtp2pcng.py,v 1.2 2004/02/25 18:49:05 ajung Exp $
+$Id: smtp2pcng.py,v 1.3 2004/02/28 10:22:05 ajung Exp $
 """
 
 import sys, os, logging, base64
+import httplib, urllib, urlparse
 from cStringIO import StringIO
 import email, email.Iterators
+from optparse import OptionParser
 from email.Header import decode_header 
 from email.MIMEText import MIMEText
 
@@ -35,7 +37,7 @@ class Result:
         IO.write('<senderaddress>%s</senderaddress>\n' % self.senderaddress)
         IO.write('<replyto>%s</replyto>\n' % self.reply_to)
         IO.write('<subject>%s</subject>\n' % self.subject)
-        IO.write('<body>%s</body>\n' % self.subject)
+        IO.write('<body>%s</body>\n' % self.body)
 
         for a in self.getAttachments():
             IO.write('<attachment mimetype="%s" filename="%s">\n' % a[1:])
@@ -74,6 +76,29 @@ def parse_mail():
 
     return R
 
+def submit_request(R, options):
+    params = urllib.urlencode({'xml': R.toXML()})
+    headers = {"Content-type": "application/x-www-form-urlencoded", 
+               "Accept": "text/plain",
+               'Authorization': 'Basic ' + base64.encodestring('%s:%s' % (options.username, options.password))[:-1],
+              }
+    f = urlparse.urlparse(options.url)
+    conn = httplib.HTTPConnection(f[1])
+
+    conn.request("POST", f[2], params, headers)
+    response = conn.getresponse()
+    print response.status, response.reason
+    data = response.read()
+    conn.close()
+
 if __name__ == '__main__':
+
+    parser = OptionParser()
+    parser.add_option('-u', '--url', dest='url', help='PloneCollectorNG URL to add issues')
+    parser.add_option('-X', '--username', dest='username', help='Plone user name')
+    parser.add_option('-P', '--password', dest='password', help='Plone user password')
+
+    options, args = parser.parse_args()
+
     R = parse_mail()
-    print R.toXML()
+    submit_request(R, options)
