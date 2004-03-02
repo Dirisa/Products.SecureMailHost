35632a5c3f83067fbc7e5b7d029f684dda6a6363
Andreas Jung <zopyxfilter@gmail.com>
2004-03-02 12:08:34 +0000

    updated

--- workflows/__init__.py
+++ workflows/__init__.py
@@ -1,1 +1,8 @@
 # placeholder
+
+from Products.Archetypes.public import DisplayList
+
+VOC_WORKFLOWS = DisplayList( [
+    ('pcng_issue_workflow', 'pcng_issue_workflow (pcng_issue_workflow)'),
+])
+
