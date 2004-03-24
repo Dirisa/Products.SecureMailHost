d1f3694225bb9f8e60e4187fed23a89512768506
Andreas Jung <zopyxfilter@gmail.com>
2004-03-24 12:22:53 +0000

    added encoding parameter

--- skins/plonecollectorng/safe_convert.py
+++ skins/plonecollectorng/safe_convert.py
@@ -1,9 +1,10 @@
-##parameters=s
+##parameters=s,enc='iso-8859-1'
 
 # Convert a string using the site-encodings
 
-enc = here.getSiteEncoding()
+site_enc = here.getSiteEncoding()
+
 if same_type(s, u''):
-    return s.encode(enc)
+    return s.encode(site_enc)
 else:
-    return unicode(s, enc, 'replace').encode(enc)
+    return unicode(s, enc, 'replace').encode(site_enc)
