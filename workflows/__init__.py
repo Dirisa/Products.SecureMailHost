975766e49c1819f1bd61e8e0374ecb9519c90529
Andreas Jung <zopyxfilter@gmail.com>
2004-03-27 04:55:09 +0000

    removed debug print

--- workflows/__init__.py
+++ workflows/__init__.py
@@ -7,7 +7,6 @@ files = [x for x in files if x.endswith('.py') and x not in ('__init__.py',)]
 lst = []
 for f in files:
     f = f[:-3]
-    print f
     mod = __import__(f, globals(), globals(), __path__)
     k = '%s (%s)' % (mod.id, mod.title)
     v = '%s (%s)' % (mod.title, mod.id)
