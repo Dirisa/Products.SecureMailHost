8e0a7fe58fa0835406ed7b0420550567c4873812
Andreas Jung <zopyxfilter@gmail.com>
2004-04-04 09:10:29 +0000

    saving search results to session

--- skins/plonecollectorng/pcng_search_results.py
+++ skins/plonecollectorng/pcng_search_results.py
@@ -56,6 +56,13 @@ if len(query) == 0:
 
 if debug: print query
 results = list(context.pcng_catalog.searchResults(query))
+
+
+try:
+    lst = [ (b.getURL(), b.getId, b.Title) for b in results]
+    R.SESSION['pcng_searchresults'] = lst
+except:
+    pass
 if sort_on: results.sort(query_sort)
 if debug: return printed
 return results
