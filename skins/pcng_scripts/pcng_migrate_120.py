aa64e523224a9bb39b43c9edf7d0caafbe94c209
Andreas Jung <zopyxfilter@gmail.com>
2004-05-09 18:42:32 +0000

    reordered

--- skins/pcng_scripts/pcng_migrate_120.py
+++ skins/pcng_scripts/pcng_migrate_120.py
@@ -1,7 +1,7 @@
 ## Migrate PloneCollectorNG instance to V 1.2.0+
 
-context.setup_tools()
 context.update_collector_schema()
+context.setup_tools()
 context.migrate_issue_workflow_histories()
 context.getTranscript().addComment(u'migrated to 1.2.0')
 
