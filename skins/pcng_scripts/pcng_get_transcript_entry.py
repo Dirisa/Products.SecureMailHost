499b3965bc639a7ab6396edd7b3281ddb746d45d
Andreas Jung <zopyxfilter@gmail.com>
2004-05-14 10:40:33 +0000

    added

---
+++ skins/pcng_scripts/pcng_get_transcript_entry.py
@@ -0,0 +1,11 @@
+##parameters=timestamp
+
+# Return a timescript entry by timestamp
+
+for ev in context.getTranscript().getEvents():
+    if ev.getType() == 'comment' and str(ev.getTimestamp()) == str(timestamp):
+        return ev.getValue('comment')
+        
+raise ValueError('No comment found for given timestamp....this should never happen')
+
+ 
