912f010c05d8106b1bda431d153f0c0d882d661f
Andreas Jung <zopyxfilter@gmail.com>
2004-06-12 09:12:17 +0000

    removed crap

--- skins/pcng_scripts/pcng_migrate_120.py
+++ skins/pcng_scripts/pcng_migrate_120.py
@@ -4,8 +4,5 @@ context.update_collector_schema()
 context.setup_tools()
 context.migrate_issue_workflow_histories()
 context.getTranscript().addComment(u'migrated to 1.2.0')
-
-for issue in context.objectValues('PloneIssueNG'):
-    issue.migrate_schema()
 context.reindex_issues()
 context.REQUEST.RESPONSE.redirect('pcng_view')
