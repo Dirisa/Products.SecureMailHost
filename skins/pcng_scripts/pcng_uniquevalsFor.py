f149338bbbeca1d69f47b2633a1faddd37e7d287
Andreas Jung <zopyxfilter@gmail.com>
2004-06-22 15:57:07 +0000

    flattening lists (from LinesFields)

--- skins/pcng_scripts/pcng_uniquevalsFor.py
+++ skins/pcng_scripts/pcng_uniquevalsFor.py
@@ -8,7 +8,12 @@ def mycmp(x, y):
     except:
         return cmp(x, y)
 
-values = context.pcng_catalog.uniqueValuesFor(key)
+values = []
+for v in context.pcng_catalog.uniqueValuesFor(key):
+    if same_type(v, []) or same_type(v, ()):
+        values.extend(v)
+    else:
+        values.append(v)
 values = [v for v in values if v]
 values.sort(mycmp)
 return values
