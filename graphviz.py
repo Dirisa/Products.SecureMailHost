5735a100f847c5ce1821deae73bb1ee4ae8e00bb
Andreas Jung <zopyxfilter@gmail.com>
2004-06-29 13:51:45 +0000

    - encoding fixes - following references

--- graphviz.py
+++ graphviz.py
@@ -5,36 +5,43 @@ PloneCollectorNG - A Plone-based bugtracking system
 
 License: see LICENSE.txt
 
-$Id: graphviz.py,v 1.10 2004/06/26 08:37:40 ajung Exp $
+$Id: graphviz.py,v 1.11 2004/06/29 13:51:45 ajung Exp $
 """
 
 ##########################################################################
 # This module creates a graphical representation of the
 # issue references using the graph drawing tool Graphviz
-# available from www.research.att.com/sw/tools/graphviz
+# available from www.research.att.com/sw/tools/graphviz                                                        
+# Also using the pydot module by Ero Carrera
 ##########################################################################
-
 
 import os, pydot, tempfile
 from urllib import unquote
 
-
-def build_graph(node, graph):
+def build_graph(node, graph, visited):
 
     def fmt(node):
-        return '%s %s' % (node.getId(), node.Title())
+        print type('%s %s' % (node.getId(), node.Title()))
+        s = '%s %s' % (node.getId(), node.Title())
+        s = unicode(s, node.getSiteEncoding(), 'ignore').encode('iso-8859-15', 'ignore')
+        return s
+
+    if not node.absolute_url(1) in visited:
+        visited.append(node.absolute_url(1))
 
     for ref in node.getForwardReferences(): 
         source = node
         target = ref.getTargetObject()
         e = pydot.Edge(fmt(source), fmt(target), label=ref.comment)
         graph.add_edge(e)
+        if not target.absolute_url(1) in visited:
+            build_graph(target, graph, visited)
 
 
 def build_tree2(issue, format):
 
     G = pydot.Dot()
-    build_graph(issue, G)
+    build_graph(issue, G, [])
     fname = tempfile.mktemp()
     writer = getattr(G, 'write_%s' % format)
     writer(fname)
