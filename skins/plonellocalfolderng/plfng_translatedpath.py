24597262ecd1db85b05550d219c2463f7642485a
mgarnsey <mgarnsey@example.org>
2004-07-19 20:57:54 +0000

    correction to previous file checkin - this is correct script to allow optional full-path vs PLFNG-relative path at top of plfng_view page

--- skins/plonellocalfolderng/plfng_translatedpath.py
+++ skins/plonellocalfolderng/plfng_translatedpath.py
@@ -5,7 +5,8 @@ url0 = context.REQUEST['URL0']
 pos = url0.find(url)
 	
 if context.diplayFullPortalPath():
-	s = url
+	urlpath0 = context.REQUEST['URLPATH0']
+	s = urlpath0
 else:
 	s = url0[pos + len(url):]
 s = s.replace('plfng_view','')
