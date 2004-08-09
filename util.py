c385e4b7694d66a7a8dbd36e6128ef8d67ec9204
mgarnsey <mgarnsey@example.org>
2004-08-09 04:03:26 +0000

    eliminated ext. syscall dependency for unzip'ing; added support for backup of files being overwritten by unzip operation

--- util.py
+++ util.py
@@ -1,33 +1,20 @@
 import os
+import shutil
 import string
 from AccessControl import ClassSecurityInfo
 from Products.CMFCore.CMFCorePermissions import ModifyPortalContent
 from ConfigParser import ConfigParser
 from Acquisition import aq_base
+import zipfile
 
 import zLOG
 
 import md5
 MD5_LENGTH = 32
 
-# Note: the unzip utility methods in this file currently depend on 
-# external system calls.  Optional support for an external md5 
-# generation system utility is also provided.
-# These methods have only been tested with the following utilities:
-#   * unzip/zipinfo: UnZip v5.50 by Info-ZIP (http://www.info-zip.org/) 
+# Note: Optional support for an external md5 generation system utility 
+# has only been tested with the following utility:
 #   * md5: md5deep v1.2 (http://md5deep.sourceforge.net/)
-# The following settings are specific to the aforementioned utilities. 
-
-# For now, locally define these system calls and expected return value results.
-# (it would be nice if these were properties!)
-
-SYSCALL_ZIPINFO         = "c:\\plone-utils\\unzip -Zt"
-SYSCALL_ZIPINFO_SUCCESS = 0
-
-SYSCALL_UNZIP           = "c:\\plone-utils\\unzip -o -d"
-SYSCALL_UNZIP_SUCCESS   = 0
-SYSCALL_TESTZIP         = "c:\\plone-utils\\unzip -qt"
-SYSCALL_TESTZIP_SUCCESS = 0
 
 security = ClassSecurityInfo()
 
@@ -77,83 +64,116 @@ def getMetadataElements(filename,section):
 
 
 # --------------------------------------------------------------------
-security.declarePublic('testZipFile')
-def testZipFile(filename):
-   syscall_filename = fixDOSPathName(filename)
-   syscall = "%s %s" % (SYSCALL_TESTZIP,syscall_filename)
-   result = os.system(syscall)
+security.declareProtected(ModifyPortalContent, 'getZipInfo')
+def getZipInfo(filename):
 
-   if result == SYSCALL_TESTZIP_SUCCESS:
-      return 1
+   filename = fixDOSPathName(filename)
+   
+   z = zipfile.ZipFile(filename) 
+   
+   uncompressedSize = 0.0
+   compressedSize = 0.0
+   for zinfo in z.filelist:
+      uncompressedSize = uncompressedSize + zinfo.file_size
+      compressedSize = compressedSize + zinfo.compress_size
+   
+   if uncompressedSize > 1.0:
+      compressionRatio = 100.0 * (1.0 - compressedSize/uncompressedSize)
    else:
-      zLOG.LOG('PloneLocalFolderNG', zLOG.INFO , "testZipFile() FAILED: %s" % syscall )
-      return 0 
+      compressionRatio = 0.0
+   
+   return len(z.filelist), uncompressedSize
+   
+   # here is the code to mimic 'unzip -Zt' output
+   # result = "%d files, %d bytes uncompressed, %d bytes compressed: %.1f%%"
+   #         % (len(z.filelist), uncompressedSize, compressedSize, compressionRatio)
+   
+   # this is the code from PLFNG 0.5 that used an external system
+   # call to the UnZip v5.50 utility.  Here is its output format: 
+   #  21 files, 50550 bytes uncompressed, 12409 bytes compressed:  75.5%
+   # -------------------------------------------------------------
+   #SYSCALL_ZIPINFO         = "c:\\plone-utils\\unzip -Zt"
+   #syscall = "%s %s" % (SYSCALL_ZIPINFO,syscall_filename)
+   #process_fp = os.popen(syscall)
+   #result = process_fp.read()
+   # -------------------------------------------------------------   
 
-# --------------------------------------------------------------------
-security.declareProtected(ModifyPortalContent, 'generate_zipinfo')
-def generate_zipinfo(filename):
-
-   syscall_filename = fixDOSPathName(filename)
-   syscall = "%s %s" % (SYSCALL_ZIPINFO,syscall_filename)
-   process_fp = os.popen(syscall)
-   result = process_fp.read()
-   return result
 
 # --------------------------------------------------------------------
 security.declareProtected(ModifyPortalContent, 'setZipInfoMetadata')
 def setZipInfoMetadata(filename):
 
-   if testZipFile(filename):
-      raw_output = generate_zipinfo(filename)
-      files,space = parseZipInfoOutput(raw_output)
-   
-      setMetadata(filename, section="ARCHIVEINFO", option="num_files", value=files)
-      setMetadata(filename, section="ARCHIVEINFO", option="unpacked_size", value=space)
+   if zipfile.is_zipfile(filename): 
+      num_files, unpacked_size = getZipInfo(filename)
+  
+      setMetadata(filename, section="ARCHIVEINFO", option="num_files", value=num_files)
+      setMetadata(filename, section="ARCHIVEINFO", option="unpacked_size", value=unpacked_size)
       return 1
 
    else:
       return 0
 
 # --------------------------------------------------------------------
-security.declareProtected(ModifyPortalContent, 'parseZipInfoOutput')
-def parseZipInfoOutput(raw_output):
-
-   FILECOUNT_STARTPOINT = 0
-   FILECOUNT_DELIMITER = " files, "
-   UNZIPSIZE_DELIMITER = " bytes uncompressed,"
-   
-   filecount_str = '0'
-   unzipsize_str = '0'
-   
-   if raw_output != None:  
-      filecount_endpoint = string.find(raw_output, FILECOUNT_DELIMITER)
-      if filecount_endpoint != -1:
-         filecount_str = raw_output[FILECOUNT_STARTPOINT:filecount_endpoint]
-         
-         unzipsize_startpoint = filecount_endpoint + len(FILECOUNT_DELIMITER)
-         unzipsize_endpoint = string.find(raw_output,UNZIPSIZE_DELIMITER)
-         if unzipsize_endpoint != -1:
-            unzipsize_str = raw_output[unzipsize_startpoint:unzipsize_endpoint]
-
-   return filecount_str,unzipsize_str
-
-# --------------------------------------------------------------------
 security.declareProtected(ModifyPortalContent, 'upzipFile') 
-def upzipFile(filename):
-   if testZipFile(filename):
-      syscall_filename = fixDOSPathName(filename)
-      exdir = os.path.dirname(syscall_filename)
-      syscall = "%s %s %s" % (SYSCALL_UNZIP,exdir,syscall_filename)
-      result = os.system(syscall)
-      if result == SYSCALL_UNZIP_SUCCESS:
-         zLOG.LOG('PloneLocalFolderNG', zLOG.INFO , "upzipFile() SUCCESS: %s" % syscall )
-         return 1
-      else:
-         zLOG.LOG('PloneLocalFolderNG', zLOG.INFO , "upzipFile() FAILED: %s" % syscall )
-         return 0 
+def upzipFile(FSfilename, FSBackupFolderBase=None):
+   # this method does the same thing as the following system call:
+   # SYSCALL_UNZIP = "c:\\plone-utils\\unzip -o -d"
+   # syscall = "%s %s %s" % (SYSCALL_UNZIP,todir,syscall_filename)
+   # result = os.system(syscall)
+
+   FSfilename = fixDOSPathName(FSfilename)
+   todir = os.path.dirname(FSfilename)
+   
+   if zipfile.is_zipfile(FSfilename): 
+
+      z = zipfile.ZipFile(FSfilename)
+
+      # create the extract directory structure before extracting files to it
+      for zitem in z.namelist():
+         if zitem.endswith('/'):
+            newDirectoryName = os.path.join(todir, zitem)
+            if not os.path.exists(newDirectoryName):
+                os.makedirs(newDirectoryName)
+
+      # extract the files from the zipfile
+      for zitem in z.namelist():
+         if not zitem.endswith('/'):
+            tofile = os.path.join(todir, zitem)
+            oldRevisionNumber = 0
+            newRevisionNumber = 1
+            # if file already exists, back it up to FSBackupFolderBase if specified
+            if os.path.exists(tofile) and FSBackupFolderBase:
+               # get revision of existing file (or 1 if revision metadata missing)
+               oldRevisionNumberText = getMetadataElement(tofile, section="GENERAL", option="revision")
+               if oldRevisionNumberText:
+                   oldRevisionNumber = int(oldRevisionNumberText)
+               else:
+                   oldRevisionNumber = 1
+
+               # move existing file to backup location renamed with trailing rev.#
+               backupdestpath = os.path.dirname(os.path.join(FSBackupFolderBase,zitem))
+               backupFileSuffix = '.' + str(oldRevisionNumber)
+               backupfilename = os.path.join(backupdestpath, os.path.basename(zitem)) + backupFileSuffix
+               # create skeleton directory structure under backupFolder if necessary 
+               if not os.path.exists(backupdestpath): os.makedirs(backupdestpath)
+               shutil.move(tofile, backupfilename)
+               #zLOG.LOG('PloneLocalFolderNG', zLOG.INFO , "upzipFile() move(%s, %s)" % (tofile,backupfilename))
+            
+            # extract the file
+            f = open(tofile, 'wb')
+            f.write(z.read(zitem))
+            f.close()
+
+            # set/update metadata 
+            newRevisionNumber = oldRevisionNumber + 1
+            setMetadata(tofile, section="GENERAL", option="revision", value=newRevisionNumber)
+
+      #zLOG.LOG('PloneLocalFolderNG', zLOG.INFO , "upzipFile() finished unzip'ing %s" % FSfilename)
+      return 1
+
    else:
-         zLOG.LOG('PloneLocalFolderNG', zLOG.INFO , "upzipFile() ABORTED: %s not a valid zip file." % filename )
-         return 0    
+      zLOG.LOG('PloneLocalFolderNG', zLOG.INFO , "upzipFile() ABORTED: %s not a valid zip file." % FSfilename )
+      return 0    
 
 # --------------------------------------------------------------------
 security.declareProtected(ModifyPortalContent, 'generate_md5')
@@ -166,16 +186,16 @@ def generate_md5(filename,syscallcommand='none'):
       m = md5.new()
       blocksize = 2<<16
       try:
-	      fobj = open(filename, 'rb')
-	      while True:
-	          chunk = fobj.read(blocksize)
-	          if not chunk:
-	              break
-	          m.update(chunk)
-	      md5_hexvalue = m.hexdigest()
-	      fobj.close()
+         fobj = open(filename, 'rb')
+         while True:
+             chunk = fobj.read(blocksize)
+             if not chunk:
+                 break
+             m.update(chunk)
+         md5_hexvalue = m.hexdigest()
+         fobj.close()
       except:
-         md5_hexvalue = '' 	
+         md5_hexvalue = ''    
 
    else:
       fullsyscall = "%s %s" % (syscallcommand,filename)
