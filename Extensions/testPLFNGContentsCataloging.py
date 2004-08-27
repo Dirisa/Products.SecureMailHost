289c8172e33a49cb9d8bf1eacf3c676dd19affb3
mgarnsey <mgarnsey@example.org>
2004-08-27 02:47:56 +0000

    bugfix + support for INSTANCE_HOME in addition to SOFTWARE_HOME installations

--- Extensions/testPLFNGContentsCataloging.py
+++ Extensions/testPLFNGContentsCataloging.py
@@ -3,7 +3,7 @@ from Products.CMFCore.utils import getToolByName
 from Products.ZCatalog.ZCatalog import manage_addZCatalog
 from Products.TextIndexNG2.TextIndexNG import manage_addTextIndexNG
 from Products.PloneLocalFolderNG import PloneLocalFolderNG
-from App.FindHomes import SOFTWARE_HOME
+from App.FindHomes import SOFTWARE_HOME,INSTANCE_HOME
 import os
 #from os.path import join
 #import string
@@ -33,11 +33,11 @@ def PLFNGCatalogTest(self):
 	
 	# create the test ZCatalog as necessary
 	if hasattr(portal, UID_CATALOG):
-		out.write("\nfound: '%s' ZCatalog" % UID_CATALOG)
+		out.write("\nfound: '%s' ZCatalog\n" % UID_CATALOG)
 	else:
 		#out.write('\n %s NOT found!!!' % UID_CATALOG)
 		manage_addZCatalog(portal,UID_CATALOG,None)
-		out.write("\nadded: '%s' ZCatalog" % UID_CATALOG)
+		out.write("\nadded: '%s' ZCatalog\n" % UID_CATALOG)
 	
 	testCatalogObj = getattr( portal, UID_CATALOG )
 
@@ -45,9 +45,9 @@ def PLFNGCatalogTest(self):
 	
 	try:
 		manage_addTextIndexNG(testCatalogObj, UID_INDEX, extra)
-		out.write("\nadded: '%s' index" % UID_INDEX)
+		out.write("\nadded: '%s' index\n" % UID_INDEX)
 	except:
-		out.write("\nfound: '%s' Index" % UID_INDEX)
+		out.write("\nfound: '%s' Index\n" % UID_INDEX)
 		pass	
 
 	#out.write('\n indices = %s' % testCatalogObj.Indexes['xSearchableText'].__class__.__name__)
@@ -56,13 +56,21 @@ def PLFNGCatalogTest(self):
 	#out.write("\ncatalogTool: %s" % catalogTool)
 	
 	if hasattr(portal, UID_PLFNG_TEST):
-	   out.write( '-!- %s already exists....leaving it alone.\n' % id )
+	   out.write("\nfound: '%s' PLFNG instance\n" % UID_PLFNG_TEST )
 	   newObj = getattr( portal, UID_PLFNG_TEST )
 	else:
-		FSpath = os.path.join(SOFTWARE_HOME,'Products')
-		FSpath = os.path.join(FSpath,'PloneLocalFolderNG')
-		FSpath = os.path.join(FSpath,'tests')
-		FSpath = os.path.join(FSpath,'SamplesFiles')+os.path.sep
+		# locate FS path to SamplesFiles subdir in INSTANCE_HOME or SOFTWARE_HOME
+		
+		FSpath = os.path.join(INSTANCE_HOME,'Products','PloneLocalFolderNG','tests','SamplesFiles')+os.path.sep
+		if os.path.exists(FSpath):
+			out.write( "\nfound: 'SamplesFiles' subdir in INSTANCE_HOME\n" )
+		else:
+			FSpath = os.path.join(SOFTWARE_HOME,'Products','PloneLocalFolderNG','tests','SamplesFiles')+os.path.sep
+			if os.path.exists(FSpath):
+				out.write( "\nfound: 'SamplesFiles' subdir in SOFTWARE_HOME\n" )
+			else: 
+				out.write( "\nERROR!!! could not find 'SamplesFiles' subdir to proxy for a test PLFNG instance.\n\n  Test aborted.\n" )
+				return out.getvalue()
 
 		portal.invokeFactory(id=UID_PLFNG_TEST, type_name='PloneLocalFolderNG')
 		newObj = getattr( portal, UID_PLFNG_TEST )
@@ -74,13 +82,9 @@ def PLFNGCatalogTest(self):
 		newObj.allow_file_unpacking = 1
 		out.write( "\nadded: '%s' PLFNG instance.\n" % UID_PLFNG_TEST )
 		
-	filesCataloged, filesNotCataloged = newObj.catalogContents(rel_dir=None, catalog=UID_CATALOG)
-	out.write("\nfilesCataloged=%d , filesNotCataloged = %d" % (filesCataloged,filesNotCataloged))	
+	filesCataloged = newObj.catalogContents(rel_dir=None, catalog=UID_CATALOG)
+	out.write("\nfilesCataloged=%d " % filesCataloged)	
 		
-	return out.getvalue()
-	
-def macPathTest(self):	
-	out=StringIO()
 	return out.getvalue()
 	
 		
