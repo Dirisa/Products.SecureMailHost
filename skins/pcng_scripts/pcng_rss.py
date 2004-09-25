35f33056325f7706cbc5ca1b8920dedf81826bb6
Andreas Jung <zopyxfilter@gmail.com>
2004-09-25 10:55:11 +0000

    *** empty log message ***

--- skins/pcng_scripts/pcng_rss.py
+++ skins/pcng_scripts/pcng_rss.py
@@ -1,7 +1,14 @@
-# Basic script to provide RSS support based on ZCatalog queries
+##parameters=**kw
+
+# Basic script to provide RSS support based on ZCatalog queries.
+# If no keyword parameters (search query) is specified then we return
+# all active issues over the last two weeks.
+
+if not kw:
+    kw = { 'last_action' :  {'query' : DateTime()-14, 'range': 'min'} }
 
 # Search the issues to be RSS-ified
-result = context.pcng_catalog(last_action = {'query' : DateTime()-14, 'range': 'min'},) 
+result = context.pcng_catalog(**kw)
 
 # Render XML
 xml = context.pcng_rss_template(result=result)
