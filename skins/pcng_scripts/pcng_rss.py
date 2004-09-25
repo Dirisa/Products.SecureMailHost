b6a53cc554bb4eabf4471095ac57f8ad957a457f
Andreas Jung <zopyxfilter@gmail.com>
2004-09-25 13:11:30 +0000

    added rss icon for search results

--- skins/pcng_scripts/pcng_rss.py
+++ skins/pcng_scripts/pcng_rss.py
@@ -1,14 +1,13 @@
-##parameters=**kw
 
 # Basic script to provide RSS support based on ZCatalog queries.
 # If no keyword parameters (search query) is specified then we return
 # all active issues over the last two weeks.
 
-if not kw:
-    kw = { 'last_action' :  {'query' : DateTime()-14, 'range': 'min'} }
+if not context.REQUEST.get('QUERY_STRING'):
+    context.REQUEST.set('last_action',  {'query' : DateTime()-14, 'range': 'min'}) 
 
 # Search the issues to be RSS-ified
-result = context.pcng_catalog(**kw)
+result = context.pcng_catalog(context.REQUEST)
 
 # Render XML
 xml = context.pcng_rss_template(result=result)
