d4fa71b23b3da2c8c15c46e871efad4ac451fc30
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2004-10-23 13:32:03 +0000

    added german translation, run merge, adapted merge to run as command

--- i18n/merge.sh
+++ i18n/merge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for PO in pfm*.po; do
-    i18ndude.py sync --pot pfm.pot -s $PO
+    i18ndude sync --pot pfm.pot -s $PO
 done
 
 for PO in plone*.po; do
-    i18ndude.py sync --pot plone-pfm.pot -s $PO
+    i18ndude sync --pot plone-pfm.pot -s $PO
 done
