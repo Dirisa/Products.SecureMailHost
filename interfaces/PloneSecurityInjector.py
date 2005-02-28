18d1333594ad6c577c1710c1a6605514b578ed62
Kapil Thangavelu <k_vertigo@users.sourceforge.net>
2005-02-28 05:10:35 +0000

    New repository initialized by cvs2svn.

--- interfaces/PloneSecurityInjector.py
+++
@@ -1,4 +0,0 @@
-from interface import Interface, Attribute
-
-class IPloneSecurityInjector(Interface):
-    """ interface for IPloneSecurityInjector """
--- interfaces/__init__.py
+++
@@ -1,3 +0,0 @@
-#interface definitions for PloneSecurityInjector
-
-
