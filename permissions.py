cc2ccc383f08119897c34db1fb69935add192944
Martin Aspeli <optilude@gmail.com>
2005-03-11 11:59:14 +0000

    Set Manager, Member, Owner to have add-content by default

--- permissions.py
+++ permissions.py
@@ -1,4 +1,9 @@
 """
-$Id: permissions.py,v 1.1 2005/03/09 18:04:32 dtremea Exp $
+$Id: permissions.py,v 1.2 2005/03/11 11:59:14 optilude Exp $
 """
+from Products.CMFCore.CMFCorePermissions import setDefaultRoles
+
 ADD_CONTENT_PERMISSION = 'Add PloneSoftwareCenter Content'
+
+# Let members add new packages for review
+setDefaultRoles(ADD_CONTENT_PERMISSION, ('Member', 'Manager','Owner',))
