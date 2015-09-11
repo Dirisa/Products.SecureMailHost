ab5320604b7aaae85e9096482f86221ee481494c
Maurits van Rees <maurits@vanrees.org>
2015-09-11 22:40:10 +0200

    Improved setup.py, point to github.

--- README.txt
+++ README.txt
@@ -29,7 +29,7 @@ License:
     ZPL 2.1
 
 Downloads:
-    http://plone.org/products/securemailhost
+    https://pypi.python.org/pypi/Products.SecureMailHost
 
 Bug collector:
-    https://dev.plone.org/plone under component Mail
+    https://github.com/collective/Products.SecureMailHost/issues
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.1.3'
+version = '1.1.3.dev0'
 
 setup(name='Products.SecureMailHost',
       version=version,
@@ -9,21 +9,29 @@ setup(name='Products.SecureMailHost',
       long_description="""\
       """,
       classifiers=[
-        "Framework :: Plone",
-        "Framework :: Zope2",
-        "Programming Language :: Python",
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Framework :: Plone :: 5.0",
+          "Framework :: Zope2",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.4",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
       ],
       keywords='Zope Plone Secure Mail Host',
       author='Christian Heimes',
       author_email='plone-developers@lists.sourceforge.net',
-      url='http://svn.plone.org/svn/collective/SecureMailHost/trunk',
+      url='https://pypi.python.org/pypi/Products.SecureMailHost',
       license='ZPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
       zip_safe=False,
-      download_url='http://plone.org/products/securemailhost/releases',
       install_requires=[
-        'setuptools',
+          'setuptools',
       ],
-)
+      )
